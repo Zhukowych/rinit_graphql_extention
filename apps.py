1072474ca3ab29df0e7af52670a21dee7812f8fe
Zhukowych <mzmzhuk@gmail.com>
2021-02-16 18:13:08 +0200

    rm some filed

--- apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MutaionsCommitsConfig(AppConfig):
-    name = 'mutaions_commits'
--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class MutationCommit(models.Model):
-    mutation_name = models.CharField(max_length=100)
-    arguments = models.TextField()
-    error = models.TextField(default=None, blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
