12954f99d349c1a27581ebcf9d6cf8b8cc76fc33
Zhukowych <mzmzhuk@gmail.com>
2021-02-16 17:40:27 +0200

    first

--- rest_books/apps/index/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-from rest_framework.authtoken import views
-
-app_name="index"
-urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('example/', TokenAuthorizationTest.as_view(), name="example"),
-]
