8e863bf117521203b67e4873cd27097ba048c40c
Zhukowych <mzmzhuk@gmail.com>
2021-02-16 19:00:32 +0200

    fix2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'rinit_graphql_extention', 
   packages = ['rinit_graphql_extention'],
-  version = '0.1',     
+  version = '0.1.2',     
   license='gpl-3.0',   
   description = 'packet to make work with graphql more comfortabke', 
   author = 'Max Zhuk',
   author_email = 'mzmzhuk@gmail.com',
   url = 'https://github.com/Zhukowych/',
-  download_url = 'https://github.com/Zhukowych/rinit_graphql_extention/archive/v_011.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Zhukowych/rinit_graphql_extention/archive/v_012.tar.gz',    # I explain this later on
   keywords = ['rinit_graphql_extention'], 
   install_requires=[          
           'django',
